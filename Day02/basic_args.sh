518949af15241494ee3bce40acaa0571729b90e4
root <root@Linux-VM.zrb4flmupgxuffwrnjl1zc4o5g.bx.internal.cloudapp.net>
2025-03-05 10:56:31 +0000

    Commiting Day01 and Day02

---
+++ Day02/basic_args.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo " first argument :  $1"
+echo " second argument :  $2"
+echo " all arguments :  $@ "
+echo  "all arguments as single string : '$*' "
+echo " Total number of arguments: $# "
+
---
+++ Day02/special_params.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "script name: $0"
+echo "all arguments: $@"
+echo " Total number of arguments : $# "
+echo " process id of the shell : $$ "
+ajsbajnvbsla
+echo " exit code of last executed command: $? "
